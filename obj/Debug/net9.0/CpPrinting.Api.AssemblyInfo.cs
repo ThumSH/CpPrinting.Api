0ea419a602641d2a183576c7ad8a33ab305f1962
Thum Hema <h3mashed@gmail.com>
2026-05-21 04:40:07 +0530

    committed before dev-test branch

--- obj/Debug/net9.0/CpPrinting.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/CpPrinting.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8497b4db5374ede12897dea0190c31f6bce25ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c968cda1c3b733c9ce372aee69b4a8c966dbd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
