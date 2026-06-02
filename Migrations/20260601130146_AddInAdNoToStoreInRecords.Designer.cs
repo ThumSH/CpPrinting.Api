5f7c3520e0511b9b6251e29821e1b37265c77b0b
Thum Hema <h3mashed@gmail.com>
2026-06-02 23:39:05 +0530

    feat:sys upgrade

--- Migrations/20260601130146_AddInAdNoToStoreInRecords.Designer.cs
+++ Migrations/20260601130146_AddInAdNoToStoreInRecords.Designer.cs
@@ -3,6 +3,7 @@ using CpPrinting.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CpPrinting.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260601130146_AddInAdNoToStoreInRecords")]
+    partial class AddInAdNoToStoreInRecords
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260601130146_AddInAdNoToStoreInRecords.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CpPrinting.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInAdNoToStoreInRecords : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InAdNo",
+                table: "StoreInRecords",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InAdNo",
+                table: "StoreInRecords");
+        }
+    }
+}
