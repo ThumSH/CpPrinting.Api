8458420f9e9209131d5d1dd552fedad6b8fe33af
Thum Hema <h3mashed@gmail.com>
2026-06-02 23:40:03 +0530

    updated sys before update 2.0

--- obj/Debug/net9.0/CpPrinting.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/CpPrinting.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58f2e9230780e3dafa6048088ef661c06677339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5052029c04ade6b6fdbda411af4ea99e6df33b9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
