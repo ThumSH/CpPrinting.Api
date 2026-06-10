ff8bf2b03b531ffe77beba7007b97c8717998d00
Thum Hema <h3mashed@gmail.com>
2026-06-10 10:03:52 +0530

    updated backend

--- obj/Debug/net9.0/CpPrinting.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/CpPrinting.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44df675bdd9c61911263ca09bde1af71fec051b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33066dd98fad3b9fafb547a29d6261917745a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CpPrinting.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
